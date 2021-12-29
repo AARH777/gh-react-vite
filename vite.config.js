707518be2c218220874acf2c37ecd85ecba69a93
Alex Reyes <73624939+AARH777@users.noreply.github.com>
2021-12-29 16:33:06 -0600

    Vite Issue resolved

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://www.alexanderreyes.me/gh-react-vite/',
   plugins: [react()]
 })
